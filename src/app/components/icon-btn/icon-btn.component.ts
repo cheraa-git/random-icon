22ae747bc5a137d66fa845ed2da9b074fd7398ec
cheraa-git <chernetsov.aa@mail.ru>
2023-05-09 20:38:33 +0400

    start develop

---
+++ src/app/components/icon-btn/icon-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-btn',
+  templateUrl: './icon-btn.component.html',
+  styleUrls: ['./icon-btn.component.css']
+})
+export class IconBtnComponent {
+
+}
